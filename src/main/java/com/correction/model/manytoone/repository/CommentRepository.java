1ef98e0b827a06b7af8e60d15599635ebe7e9693
khalil <khalillakhdharatc@gmail.com>
2021-11-20 17:34:28 +0100

    Update CommentRepository.java

--- src/main/java/com/correction/model/manytoone/repository/CommentRepository.java
+++ src/main/java/com/correction/model/manytoone/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package com.correction.model.manytoone.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.correction.model.manytoone.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+List<Comment> findByPostId(long postId);
+Optional<Comment> findByIdAndPostId(long id,long postId);
 }
